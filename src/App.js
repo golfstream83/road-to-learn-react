29c53b3a587def6a7f954b723f79c183b0e31bda
victul <victul83@gmail.com>
2018-11-11 10:02:48 +0300

    button is made by a separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const list = [
@@ -95,16 +94,32 @@ class Table extends Component {
                         <span>{item.num_comments}</span>
                         <span>{item.points}</span>
                         <span>
-                            <button
-                                onClick={() => onDismiss(item.objectID)}
-                                type="button"
-                            >
+                            <Button onClick={() => onDismiss(item.objectID)}>
                                 Dismiss
-                            </button>
+                            </Button>
                         </span>
                     </div>
                 )}
             </div>
+        );
+    }
+}
+
+class Button extends Component {
+    render() {
+        const {
+            onClick,
+            className = '',
+            children,
+        } = this.props;
+        return (
+            <button
+                onClick={onClick}
+                className={className}
+                type="button"
+            >
+                {children}
+            </button>
         );
     }
 }
