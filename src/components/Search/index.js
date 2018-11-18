34c7e83f71d116e2baf2f358747c42afb7ab81f7
victul <victul83@gmail.com>
2018-11-18 09:07:30 +0300

    added focus on input element using Ref

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-export const Search = ({
-                    value,
-                    onChange,
-                    onSubmit,
-                    children
-                }) =>
-    <form onSubmit={onSubmit}>
-        <input
-            type="text"
-            value={value}
-            onChange={onChange}
-        />
-        <button type="submit">
-            {children}
-        </button>
-    </form>;
+export class Search extends Component {
+    componentDidMount() {
+        if (this.input) {
+            this.input.focus();
+        }
+    }
+
+    render() {
+        const {
+            value,
+            onChange,
+            onSubmit,
+            children
+        } = this.props;
+
+        return (
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                    ref={el => this.input = el}
+                />
+                <button type="submit">
+                    {children}
+                </button>
+            </form>
+        );
+    }
+}
 
 Search.propTypes = {
     value: PropTypes.string,
